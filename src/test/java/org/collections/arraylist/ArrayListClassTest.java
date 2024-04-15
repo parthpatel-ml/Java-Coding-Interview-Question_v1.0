43122bf83d44eff6e04e3ba4f219c68dcdfe0347
parthpatel-ml <parthpatel.ml@gmail.com>
2024-04-15 19:07:12 +0930

    Some commits

--- src/test/java/org/collections/arraylist/ArrayListClassTest.java
+++ src/test/java/org/collections/arraylist/ArrayListClassTest.java
@@ -29,12 +29,6 @@ public class ArrayListClassTest {
             System.out.println(i);
         }
 
-        /** Question 1:
-         * All collection classes of java store memory location of the objects they collect.
-         * The primitive values do not fit in to the same definition.
-         * List<int> int = new java.util.ArrayList<>(5);
-         * */
-
         BiFunction<Float,Float,Float> abc= (a,b)-> a+b;
         System.out.println(abc.apply(10f,10f));
     }
